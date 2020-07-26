578b4ae0737b8217c0a7b048c87d64c6b02b64fb
Juan Carlos Aragón <juank1316@gmail.com>
2020-07-25 18:01:57 -0600

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,6 @@
+complete_data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data <- subset(complete_data,Date %in% c('1/2/2007','2/2/2007'))
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+hist(data$Global_active_power,col = "red",main="Global Active Power",xlab = "Global Active Power (kilowatts)")
+dev.copy(png,file="plot1.png", width=480, height=480)
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,13 @@
+complete_data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data <- subset(complete_data,Date %in% c('1/2/2007','2/2/2007'))
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+with(data,{
+  plot(Sub_metering_1~Datetime,type="l",ylab="Energy sub metering")
+  lines(Sub_metering_2~Datetime,col="red")
+  lines(Sub_metering_3~Datetime,col="blue")
+})
+legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, lwd=2)
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
